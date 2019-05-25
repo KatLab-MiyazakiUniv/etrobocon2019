ea744b91c8cd777eea268c48a606a977cabf5bb8
Futa <korosuke613613@gmail.com>
2019-05-25 15:46:52 +0900

    メインタスクの中身を切り分けた。

---
+++ src/EtRobocon2019.h
@@ -0,0 +1,9 @@
+#ifndef __ETROBOCON2019__
+#define __ETROBOCON2019__
+
+class EtRobocon2019 {
+ public:
+  static void start();
+};
+
+#endif
--- src/app.cpp
+++ src/app.cpp
@@ -1,7 +1,5 @@
 #include "app.h"
-
-#include "Controller.h"
-#include "Display.h"
+#include "EtRobocon2019.h"
 
 // 演習用のユーティリティ
 
@@ -10,36 +8,7 @@
  */
 // tag::main_task_1[]
 void main_task(intptr_t unused) {
-  Controller controller;
-  const int pwm = (Controller::MOTOR_PWM_MAX) / 6;
-  const int duration = 2000;
-
-  Display::init();
-  while(1) {
-    Display::print(1, "Forwarding...");
-    controller.leftWheel.setPWM(pwm);
-    controller.rightWheel.setPWM(pwm);
-    controller.clock.sleep(duration);
-// end::main_task_1[]
-// tag::main_task_2[]
-    Display::print(1, "Backwarding...");
-    controller.leftWheel.setPWM(-pwm);
-    controller.rightWheel.setPWM(-pwm);
-    controller.clock.sleep(duration);
-
-    // 左ボタンを長押し、それを捕捉する
-    if (controller.buttonIsPressedLeft()) {
-      break;
-    }
-  }
-
-  Display::print(1, "Stopped...");
-  controller.leftWheel.stop();
-  controller.rightWheel.stop();
-  while(controller.buttonIsPressedLeft()) {
-    ;
-  }
-
+  EtRobocon2019::start();
   ext_tsk();
 }
 // end::main_task_2[]
