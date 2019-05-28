2814ff452438d77bf0c03509428a916de89febf7
Futa <korosuke613613@gmail.com>
2019-05-28 23:14:33 +0900

    コメントを修正

--- test/EtRobocon2019Test.cpp
+++ test/EtRobocon2019Test.cpp
@@ -1,12 +1,3 @@
-/**
- * SelfLocalizationTest.cpp
- */
-
-/* コンパイル(平木場)
-$ g++-7 NormalCourseTest.cpp ../src/NormalCourse.cpp gtest_main.o gtest-all.o -I../include
--I../../googletest/googletest/include
-*/
-
 #include "EtRobocon2019.h"  // このヘッダファイルのcppファイルをテスト
 #include <gtest/gtest.h>
 
