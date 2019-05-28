63d9b0d993c9aeeb52b59dcfa0479ed4e980a03f
Futa <korosuke613613@gmail.com>
2019-05-28 17:34:26 +0900

    fix

---
+++ test/EtRobocon2019Test.cpp
@@ -0,0 +1,27 @@
+/**
+ * SelfLocalizationTest.cpp
+ */
+
+/* コンパイル(平木場)
+$ g++-7 NormalCourseTest.cpp ../src/NormalCourse.cpp gtest_main.o gtest-all.o -I../include
+-I../../googletest/googletest/include
+*/
+
+#include "EtRobocon2019.h"  // このヘッダファイルのcppファイルをテスト
+#include <gtest/gtest.h>
+
+namespace etrobocon2018_test {
+
+  class EtRobocon2019Test : public ::testing::Test {
+    friend class EtRobocon2019;
+
+   protected:
+    virtual void SetUp() {}
+    EtRobocon2019 et;
+  };
+
+  TEST_F(EtRobocon2019Test, startTest)
+  {
+    et.start();
+  }
+}  // namespace etrobocon2018_test
