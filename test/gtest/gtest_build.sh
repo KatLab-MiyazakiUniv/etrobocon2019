6a21c41d6ab14378a9f53fd14ed011c0382cede9
Futa <korosuke613613@gmail.com>
2019-05-28 19:21:47 +0900

    fix

--- test/gtest/gtest_build.sh
+++ test/gtest/gtest_build.sh
@@ -4,12 +4,14 @@ if [ -e product ]; then
     cd product
 fi
 
+set -eu
+
 rm -rf build
 mkdir build
 cd build
 
-set -eu
 
 cmake -DCMAKE_BUILD_TYPE=Coverage ..
 cmake --build .
-make
+export GTEST_COLOR=1
+ctest -VV
--- test/gtest/gtest_start.sh
+++ test/gtest/gtest_start.sh
@@ -6,5 +6,10 @@ if [ -e product ]; then
     cd product
 fi
 
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=Coverage ..
 cmake --build .
-make
+
+export GTEST_COLOR=1
+ctest -VV
