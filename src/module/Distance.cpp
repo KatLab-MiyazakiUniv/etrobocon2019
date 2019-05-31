db96006c9c45255e45b131b609c4aa3c3269c754
Oiwane Yuki <drhv8272@gmail.com>
2019-05-31 18:33:29 +0900

    Distanceクラスを修正

--- src/module/Distance.cpp
+++ src/module/Distance.cpp
@@ -12,10 +12,9 @@ Distance::Distance()
 
 /**
  * @brief 走行距離を計算して戻り値として返す
- * @param wheel [タイヤのオブジェクト]
+ * @param angle[タイヤのモーター角位置]
  * @return 走行距離
  */
 double Distance::getDistance(int angle){
-  // double angle = static_cast<double>(getAngle(wheel));
   return 2.0 * M_PI * radius * angle / 360.0;
 }
