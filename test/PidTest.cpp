982b0b7e4e1e832d0d0ac70c00b0a94707cdc097
T.Miyaji <thomath0901@gmail.com>
2019-05-31 16:11:50 +0900

    Pidクラスをテストするファイルを追加

---
+++ test/PidTest.cpp
@@ -0,0 +1,30 @@
+/**
+ *  @file   PidTest.cpp
+ *  @brief  PID制御クラスのテストファイル
+ *  @author T.Miyaji
+ */
+#include "Pid.h"
+#include <gtest/gtest.h>
+
+namespace etrobocon2019_test {
+  TEST(Pid, Pid_init)
+  {
+    // P制御を行なうオブジェクトの作成
+    Pid p(80, 0.6);
+
+    // PI制御を行なうオブジェクトの作成
+    Pid pi(80, 0.6, 0.05);
+
+    // PID制御を行なうオブジェクトの作成
+    Pid pid(80, 0.6, 0.05, 0.04);
+  }
+
+  TEST(Pid, pControl)
+  {
+    // P制御を行なうオブジェクトの作成
+    Pid p(80, 0.6);
+    float sensor = 60;
+    float expected = (80 - sensor) * 0.6;
+    ASSERT_FLOAT_EQ(expected, p.control(sensor));
+  }
+}  // namespace etrobocon2019_test
