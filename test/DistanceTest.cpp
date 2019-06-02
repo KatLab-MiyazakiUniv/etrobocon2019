b4447831c3fadff56c5f42eed50dbe8c88d769ee
Oiwane <drhv8272@gmail.com>
2019-06-02 16:56:57 +0900

    テストコードを追加

--- test/DistanceTest.cpp
+++ test/DistanceTest.cpp
@@ -2,9 +2,14 @@
 #include <gtest/gtest.h>
 
 namespace {
-  TEST(DistanceTest, testgetAngle)
+  TEST(DistanceTest, testgetAngle1)
   {
     current_angle = 10;
+    pre_angle = 5;
+  }
+  TEST(DistanceTest, testgetAngle2)
+  {
+    current_angle = -10;
     pre_angle = 5;
   }
 }
