fc4b85fba8f4287f80f0ff3a0b868955c8863134
Futa <korosuke613613@gmail.com>
2019-06-02 18:02:27 +0900

    キャリブレータークラスのヘッダファイルを作成

---
+++ src/module/Calibrator.h
@@ -0,0 +1,42 @@
+/**
+ *  @file   Calibrator.h
+ *  @brief  キャリブレーションを行うクラス
+ *  @author korosuke613
+ **/
+#ifndef CALIBRATOR_H
+#define CALIBRATOR_H
+
+#include "Controller.h"
+
+class Calibrator {
+ public:
+  Calibrator();
+
+  /** キャリブレーションを行う。
+   * @return キャリブレーションが成功したかどうかの真理値(Trueなら正常終了)
+   */
+  bool calibration();
+
+  /** Leftコースであるかどうかを判断する。
+   * @return Leftコースであるかどうかの真偽値（TrueならLeftコース）
+   */
+  bool isLeftCource();
+
+  /** 白色の値を取得する。
+   * @return 明るさ
+   */
+  int getWhiteBrightness();
+
+  /** 黒色の値を取得する。
+   * @return 明るさ
+   */
+  int getBlackBrightness();
+
+
+ private:
+  bool isLeft = true;  //Leftコースであるかどうかの真偽値（TrueならLeftコース）
+  unsigned int brightnessOfWhite = 0;  //白色の明るさ
+  unsigned int brightnessOfBlack = 0;  //黒色の明るさ
+};
+
+#endif
