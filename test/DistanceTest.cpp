11308a6eef3c5f61bbff1474084b0e25711755b2
Oiwane <44086176+Oiwane@users.noreply.github.com>
2019-06-02 16:58:53 +0900

    Update DistanceTest.cpp

--- test/DistanceTest.cpp
+++ test/DistanceTest.cpp
@@ -9,7 +9,20 @@
 #include <cmath>
 
 namespace etrobocon2019_test{
-  TEST(Distance, getDistanceTest)
+  TEST(Distance, getDistanceTest1)
+  {
+    Distance dist;
+    Controller controller;
+    const int radius = 50;
+
+    int leftAngle = 10;
+    int rightAngle = 11;
+    double angle = static_cast<double>(leftAngle + rightAngle) / 2.0;
+    double expected = 2.0 * M_PI * radius * angle / 360.0;
+  
+    ASSERT_EQ(expected, dist.getDistance(leftAngle, rightAngle));
+  }
+  TEST(Distance, getDistanceTest2)
   {
     Distance dist;
     Controller controller;
