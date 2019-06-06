741ff460b08a30bbc955f288fae2da965eec34e4
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-06-07 01:00:57 +0900

    Change: 生配列から固定長配列に変更とtemplateによる配列サイズの指定

--- src/module/NormalCource.cpp
+++ src/module/NormalCource.cpp
@@ -1,21 +1,22 @@
 /**
- *  @file   NormalCource.cpp
+ *  @file   NormalCource.h
  *  @brief  NormalCourceを走る
  *  @author Tatsumi Nishida
  */
 #include "NormalCource.h"
 
-void NormalCource::selectedEdgeLR(Edge &selectedEdge_) {
-  if (selectedEdge_ == Edge::LEFT) {
+void NormalCource::selectedEdgeLR(Edge& selectedEdge_)
+{
+  if(selectedEdge_ == Edge::LEFT) {
     selectedEdge = false;
   } else {
     selectedEdge = true;
   }
 }
 
-void NormalCource::lineTraceForSpecifiedDistance(
-    NormalCourceProperty &normalCourceProperty[]) {
-  for (auto &ncp : normalCourceProperty) {
-    ncp.speed
+void NormalCource::lineTraceForSpecifiedDistance(std::array<NormalCourceProperty, N> normalCourceProperty)
+{
+  for(auto& ncp : normalCourceProperty) {
+    lineTrace(ncp);
   }
 }
--- src/module/NormalCource.h
+++ src/module/NormalCource.h
@@ -6,13 +6,15 @@
 #ifndef NORMALCOURCE_H
 #define NORMALCOURCE_H
 
-#include "LineTracer.h" // 仮のヘッダファイル
+#include "LineTracer.h"  // 仮のヘッダファイル
 #include "NormalCourceProperty.h"
+#include <array>
+
 // #include "Controller.h"
 enum struct Edge { LEFT, RIGHT };
 
 class NormalCource {
-private:
+ private:
   //! エッジがどっちか． falseが左エッジ，trueが右エッジ
   bool selectedEdge;
   //! ライントレースのインスタンス
@@ -20,10 +22,13 @@ private:
   //! 明るさ
   int targetBrightness;
 
-public:
-  //! コンストラクタ
-  NormalCource(Edge &selectedEdge_, int targetBrightness_)
-      : targetBrightness(targetBrightness_) {
+  template <size_t N>
+
+  public :
+      //! コンストラクタ
+      NormalCource(Edge& selectedEdge_, int targetBrightness_)
+    : targetBrightness(targetBrightness_)
+  {
     NormalCource::selectedEdgeLR(selectedEdge_);
     lineTrace(selectedEdge, targetBrightness);
   }
@@ -32,7 +37,7 @@ public:
    * 左エッジ，右エッジを切り替える．
    * @param Edge::LEFT or, Edge::RIGHT
    */
-  void selectedEdgeLR(Edge &selectedEdge_);
+  void selectedEdgeLR(Edge& selectedEdge_);
 
   /**
    * 引数として受け取った距離だけライントレースする．
@@ -40,8 +45,7 @@ public:
    * @param target 黒色と白色の境界の輝度値
    * @param pwm モータパワー
    */
-  void
-  lineTraceForSpecifiedDistance(NormalCourceProperty &normalCourceProperty);
+  void lineTraceForSpecifiedDistance(std::array<NormalCourceProperty, N> normalCourceProperty);
 };
 
 #endif
