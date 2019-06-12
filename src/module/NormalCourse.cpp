f6f55385b3c3d794ec87e9893eb8c611494975f5
Tatsumi0000 <ti18027@student.miyazaki-u.ac.jp>
2019-06-12 12:17:35 +0900

    Fix: templateの使い方の修正

--- src/module/NormalCourse.cpp
+++ src/module/NormalCourse.cpp
@@ -5,31 +5,58 @@
  */
 #include "NormalCourse.h"
 
+/** コンストラクタ
+ *  @param selectedEdge_  [Leftコースである場合True]
+ *  @param targetBrightness_ [カラーセンサーの目標値]
+ */
+NormalCourse::NormalCourse(bool selectedEdge_, int targetBrightness_)
+  : selectedEdge(selectedEdge_),
+    targetBrightness(targetBrightness_),
+    lineTracer(targetBrightness, selectedEdge)
+{
+}
+
 void NormalCourse::selectedEdgeLR(bool selectedEdge_)
 {
   selectedEdge = selectedEdge_;
-  runNormalCourse();
+  NormalCourse::runNormalCourse();
 }
 
-void NormalCourse::lineTraceForSpecifiedDistance(
-    std::array<NormalCourseProperty, SIZE>& normalCourseProperty)
+template <typename std::size_t N>
+void NormalCourse::lineTraceForSpecifiedDistance(std::array<NormalCourseProperty, N>& ncpArray)
 {
-  for(auto& ncp : normalCourseProperty) {
-    lineTrace.run(ncp);
+  for(auto& ncp : ncpArray) {
+    lineTracer.run(ncp);
   }
 }
 
-void NormalCourse::lineTraceForSpecifiedDistance(NormalCourseProperty& normalCourseProperty)
-{
-  lineTrace.run(normalCourseProperty);
-}
+// void NormalCourse::lineTraceForSpecifiedDistance(NormalCourseProperty& normalCourseProperty)
+// {
+//   lineTracer.run(normalCourseProperty);
+// }
 
 void NormalCourse::runNormalCourse()
 {
-  // ライントレースのインスタンスを生成
-  LineTracer lineTrace = LineTrace(selectedEdge, targetBrightness);
-  // ここからパラメータを頑張って書く．
+  // ここにパラメータを頑張って書く．
   // 配列の個数
-  const int size = 8;
-  
+  const int arraySize = 5;
+  std::array<PidParameter, arraySize> speedPid = { { { 0.1, 0.2, 0.3 },
+                                                     { 0.4, 0.5, 0.6 },
+                                                     { 0.7, 0.8, 0.9 },
+                                                     { 1.0, 1.1, 1.2 },
+                                                     { 1.3, 1.4, 1.5 } } };
+  std::array<PidParameter, arraySize> turnPid = { { { 1.6, 1.7, 1.8 },
+                                                    { 1.9, 2.0, 2.1 },
+                                                    { 2.2, 2.3, 2.4 },
+                                                    { 2.5, 2.5, 2.6 },
+                                                    { 2.7, 2.8, 2.9 } } };
+  std::array<NormalCourseProperty, arraySize> normalCourseProperty = { {
+      { 101, 102, speedPid[0], turnPid[0] },
+      { 103, 104, speedPid[1], turnPid[1] },
+      { 105, 106, speedPid[2], turnPid[2] },
+      { 107, 108, speedPid[3], turnPid[3] },
+      { 109, 110, speedPid[4], turnPid[4] },
+  } };
+  lineTraceForSpecifiedDistance(normalCourseProperty);
+  // lineTraceForSpecifiedDistance();
 }
--- src/module/NormalCourse.h
+++ src/module/NormalCourse.h
@@ -5,29 +5,20 @@
  */
 #ifndef NORMALCOURSE_H
 #define NORMALCOURSE_H
-
-#include "LineTracer.h"  // 仮のヘッダファイル
 #include <array>
+#include "LineTracer.h"
 
 class NormalCourse {
- private:
-  //! 配列の要素数（コンパイル時に解決）
-  template <std::size_t SIZE>
-  //! エッジがどっちか． trueが左エッジ，falseが右エッジ
-  bool selectedEdge;
-  //! 明るさ
-  int targetBrightness;
-
  public:
-  //! コンストラクタ
-  NormalCourse(bool selectedEdge_, int targetBrightness_)
-    : targetBrightness(targetBrightness_), selectedEdge(false)
-  {
-  }
+  /** コンストラクタ
+   *  @param selectedEdge_  [Leftコースである場合True]
+   *  @param targetBrightness_ [カラーセンサーの目標値]
+   */
+  NormalCourse(bool selectedEdge_, int targetBrightness_);
 
   /**
    * 左エッジ，右エッジを切り替える．
-   * @param Edge::LEFT or, Edge::RIGHT
+   * @param selectedEdge_ [Leftコースである場合True]
    */
   void selectedEdgeLR(bool selectedEdge_);
 
@@ -35,18 +26,27 @@ class NormalCourse {
    * 引数として受け取った距離だけライントレースする．（配列バージョン）
    * @param normalCourseProperty NormalCourseProperty構造体の配列
    */
-  void lineTraceForSpecifiedDistance(std::array<NormalCourseProperty, SIZE>& normalCourseProperty);
+  template <typename std::size_t N>
+  void lineTraceForSpecifiedDistance(std::array<NormalCourseProperty, N>& ncpArray);
 
   /**
    * 引数として受け取った距離だけライントレースする．（配列じゃないバージョン）
    * @param normalCourseProperty NormalCourseProperty構造体
    */
-  void lineTraceForSpecifiedDistance(NormalCourseProperty& normalCourseProperty);
+  // void lineTraceForSpecifiedDistance(NormalCourseProperty& normalCourseProperty);
 
   /**
    * 実際にNormalコースを走る．
    */
   void runNormalCourse();
+
+ private:
+  //! エッジがどっちか． trueが左コース，falseが右コース
+  bool selectedEdge;
+  //! 目標の輝度値
+  int targetBrightness;
+  //!
+  LineTracer lineTracer;
 };
 
 #endif
