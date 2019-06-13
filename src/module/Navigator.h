5da767fd9f0fab5f9c7069852ae2dc506fb3285c
harakai <harakai810@yahoo.co.jp>
2019-06-13 16:24:09 +0900

    Navigator.hの変更

--- src/module/Navigator.h
+++ src/module/Navigator.h
@@ -11,18 +11,17 @@
 #include "Distance.h"
 #include <cmath>
 
-class Navigator
-{
-public:
-   explicit Navigator(Controller &controller_);
-   void move(int specifiedValue, int pwm = 30);
+class Navigator {
+ public:
+  explicit Navigator(Controller& controller_);
+  void move(int specifiedValue, int pwm = 30);
 
-private:
-   Distance distance;
-   Controller &controller;
-   void forward(int specifiedValue, int pwm);
-   void backward(int specifiedValue, int pwm);
-   void isMoved(int goalDistance, bool isForward);
+ private:
+  Distance distance;
+  Controller& controller;
+  void forward(int specifiedValue, int pwm, int goalDistance);
+  void backward(int specifiedValue, int pwm, int goalDistance);
+  bool isMoved(int goalDistance, bool isForward);
 };
 
 #endif
