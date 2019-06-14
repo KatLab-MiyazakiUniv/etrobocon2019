c4aad697e9320812af90cfa302f0a4e23513746c
Futa Hirakoba <korosuke613613@gmail.com>
2019-06-14 18:33:02 +0000

    スクリプトをdevcontainerに対応

--- gtest_instant.sh
+++ gtest_instant.sh
@@ -2,4 +2,8 @@
 
 set -eu
 
-docker run --rm -it -v "${PWD}":/home/hrp2/sdk/workspace/product korosuke613/etrobo-docker /home/hrp2/sdk/workspace/product/test/gtest/gtest_start.sh
+if hash docker 2>/dev/null; then
+    docker run --rm -it -v "${PWD}":/home/hrp2/sdk/workspace/product korosuke613/etrobo-docker /home/hrp2/sdk/workspace/product/test/gtest/gtest_start.sh
+else
+    ./test/gtest/gtest_start.sh
+fi
