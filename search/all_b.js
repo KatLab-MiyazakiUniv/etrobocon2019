82ff5f23ed0f533806906dc6197ee331d315b0c5
katlabtab <katlabtab@gmail.com>
2019-06-17 11:50:46 +0000

    Update [ci skip]

--- search/all_b.js
+++ search/all_b.js
@@ -5,5 +5,6 @@ var searchData=
   ['makefile_2einc',['Makefile.inc',['../_makefile_8inc.html',1,'']]],
   ['motor_5fpwm_5fmax',['MOTOR_PWM_MAX',['../class_controller.html#a41bfe4231d282b5e15516d35ad327150',1,'Controller']]],
   ['motor_5fpwm_5fmin',['MOTOR_PWM_MIN',['../class_controller.html#a1d287382c61600efd21be5be50d9aaba',1,'Controller']]],
-  ['move',['move',['../class_navigator.html#abbf56b48c4f42f4d006dd2637927421a',1,'Navigator']]]
+  ['move',['move',['../class_navigator.html#abbf56b48c4f42f4d006dd2637927421a',1,'Navigator']]],
+  ['movebypid',['moveByPid',['../class_navigator.html#ace0ed497062e8f9504a26af3b9b8617d',1,'Navigator']]]
 ];
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['main_5ftask',['main_task',['../app_8cpp.html#a4754344fc3fec0a802660bb5dcdefad9',1,'main_task(intptr_t unused):&#160;app.cpp'],['../app_8h.html#ab874c34d9d66b14d189a9a3f2d065991',1,'main_task(intptr_t exinf):&#160;app.cpp']]],
-  ['move',['move',['../class_navigator.html#abbf56b48c4f42f4d006dd2637927421a',1,'Navigator']]]
+  ['move',['move',['../class_navigator.html#abbf56b48c4f42f4d006dd2637927421a',1,'Navigator']]],
+  ['movebypid',['moveByPid',['../class_navigator.html#ace0ed497062e8f9504a26af3b9b8617d',1,'Navigator']]]
 ];
