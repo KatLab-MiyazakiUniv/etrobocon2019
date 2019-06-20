7aeb2764e9c27f1015d17f33dd5565e6ab329338
T.Miyaji <thomath0901@gmail.com>
2019-06-20 07:02:06 +0000

    Loggerクラスのテストファイルを作成した

---
+++ test/LoggerTest.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file   LoggerTest.cpp
+ * @brief  LoggerTestクラスのテストファイル
+ * @author T.Miyaji
+ */
+#include <gtest/gtest.h>
+#include "Logger.h"
+
+namespace etrobocon2019_test {
+  TEST(Logger, construct) { Logger logger(); }
+}  // namespace etrobocon2019_test
