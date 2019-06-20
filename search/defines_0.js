4c32cff21f474e9bcdee3d684b9e4c23d97c11b1
katlabtab <katlabtab@gmail.com>
2019-06-20 11:21:26 +0000

    Update [ci skip]

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5fuse_5fmath_5fdefines',['_USE_MATH_DEFINES',['../_distance_8cpp.html#a525335710b53cb064ca56b936120431e',1,'Distance.cpp']]]
+  ['_5fuse_5fmath_5fdefines',['_USE_MATH_DEFINES',['../_distance_8h.html#a525335710b53cb064ca56b936120431e',1,'Distance.h']]]
 ];
