83eda9fafbb60a246a435baf5f8f14ee565ee05f
T.Miyaji <thomath0901@gmail.com>
2019-06-20 07:15:08 +0000

    write関数のテストを書いた

--- src/module/Logger.h
+++ src/module/Logger.h
@@ -19,11 +19,23 @@ class Logger {
    * @param mode [ファイルの書き込みモード（デフォルトでは追記）]
    */
   Logger(const char* mode = "a");
+
   /**
    * @brief ファイルポインタを解放するデストラクタ
    */
   ~Logger();
+
+  /**
+   * @brief 出力ファイル名を返す
+   */
   std::string getFileName();
+
+  /**
+   * @brief 指定したデータを出力ファイルに書き込む
+   * @detail Usage: foo.write("%d,%f,%s\n", x, y, z);
+   * @param format [書式指定子]
+   */
+
   bool write(const char* format, ...);
 };
 
--- test/LoggerTest.cpp
+++ test/LoggerTest.cpp
@@ -4,8 +4,23 @@
  * @author T.Miyaji
  */
 #include <gtest/gtest.h>
+#include <fstream>
 #include "Logger.h"
 
 namespace etrobocon2019_test {
-  TEST(Logger, construct) { Logger logger(); }
+  TEST(Logger, construct) { Logger logger; }
+
+  TEST(Logger, writeTest)
+  {
+    Logger logger;
+    // ファイルが正しく書き込めることを確認する
+    ASSERT_TRUE(logger.write("%d\n", 123));
+
+    // 出力ファイルが存在することを確認する
+    std::ifstream ifs("log.csv");
+    ASSERT_TRUE(ifs.is_open());
+
+    // 作成したファイルを削除する
+    std::remove("log.csv");
+  }
 }  // namespace etrobocon2019_test
