c0cf4c829def8f530155c207ccd1ac9774b99102
katlabtab <katlabtab@gmail.com>
2019-06-20 11:19:15 +0000

    Update [ci skip]

--- search/all_10.js
+++ search/all_10.js
@@ -4,7 +4,7 @@ var searchData=
   ['targetspeed',['targetSpeed',['../struct_normal_course_property.html#a88f22b1a08b7abc25e2590dd4bfafe0c',1,'NormalCourseProperty']]],
   ['touchsensor',['touchSensor',['../class_controller.html#a5e19c12fa264d296fafb2996459ddae2',1,'Controller']]],
   ['tslptsk',['tslpTsk',['../class_controller.html#a2c68b1076bc4d456d69e92075a141cb3',1,'Controller']]],
-  ['turncontrol',['TurnControl',['../class_turn_control.html',1,'TurnControl'],['../class_turn_control.html#aa517e31db70e27de33c21143b1de110a',1,'TurnControl::TurnControl()']]],
+  ['turncontrol',['TurnControl',['../class_turn_control.html',1,'TurnControl'],['../class_turn_control.html#ae492d3a93446b94e889c90633447ab94',1,'TurnControl::TurnControl()']]],
   ['turncontrol_2ecpp',['TurnControl.cpp',['../_turn_control_8cpp.html',1,'']]],
   ['turncontrol_2eh',['TurnControl.h',['../_turn_control_8h.html',1,'']]],
   ['turnpid',['turnPid',['../struct_normal_course_property.html#a158da10a20be69329982502c70cba769',1,'NormalCourseProperty']]]
