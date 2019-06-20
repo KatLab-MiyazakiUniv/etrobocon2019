73ea691e742f21e80247b6b7db82898590c3ddf5
T.Miyaji <thomath0901@gmail.com>
2019-06-20 09:56:59 +0000

    Loggerの<<演算子に関するテストを書いた

--- test/LoggerTest.cpp
+++ test/LoggerTest.cpp
@@ -5,7 +5,11 @@
  */
 #include <gtest/gtest.h>
 #include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 #include "Logger.h"
+#include <iostream>
 
 namespace etrobocon2019_test {
   TEST(Logger, construct) { Logger logger; }
@@ -15,6 +19,21 @@ namespace etrobocon2019_test {
     Logger logger;
     // ファイルが正しく書き込めることを確認する
     logger.write("%d\n", 123);
+
+    // 出力ファイルが存在することを確認する
+    std::ifstream ifs("log.csv");
+    ASSERT_TRUE(ifs.is_open());
+
+    // 作成したファイルを削除する
+    std::remove("log.csv");
+  }
+
+  TEST(Logger, streamTest)
+  {
+    Logger logger;
+
+    logger << 1 << "," << 2.3 << ","
+           << "a\n";
 
     // 出力ファイルが存在することを確認する
     std::ifstream ifs("log.csv");
