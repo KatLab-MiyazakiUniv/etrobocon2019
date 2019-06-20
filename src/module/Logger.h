2494e54dd0dfe1be79292c5fd90f9ec076feb9f1
T.Miyaji <thomath0901@gmail.com>
2019-06-20 11:52:07 +0000

    <<演算子の関数にコメントを付けた

--- src/module/Logger.h
+++ src/module/Logger.h
@@ -37,9 +37,13 @@ class Logger {
    * @detail Usage: foo.write("%d,%f,%s\n", x, y, z);
    * @param format [書式指定子]
    */
-
   void write(const char* format, ...);
 
+  /**
+   * @brief 整数型のデータを出力ファイルに書き込む
+   * @detail Usage: foo << 1 << 2 << 3; (自動的にデリミターと改行が挿入される)
+   * @param intValue [整数型(int, unsigned int, std::int8_t, etc...)]
+   */
   template <typename T,
             typename std::enable_if<std::is_integral<T>::value, std::nullptr_t>::type = nullptr>
   Logger& operator<<(T intValue)
@@ -48,6 +52,11 @@ class Logger {
     return *this;
   }
 
+  /**
+   * @brief 浮動小数点数型のデータを出力ファイルに書き込む
+   * @detail Usage: foo << 1.2 << 2.4 << 3.98; (自動的にデリミターと改行が挿入される)
+   * @param floatingPointValue [浮動小数点数型(float, double)]
+   */
   template <typename T,
             typename std::enable_if<std::is_floating_point<T>::value, std::nullptr_t>::type
             = nullptr>
@@ -57,6 +66,11 @@ class Logger {
     return *this;
   }
 
+  /**
+   * @brief 文字列を出力ファイルに書き込む
+   * @detail Usage: foo << "aa" << "bb" (自動的にデリミタ―と改行が挿入される)
+   * @param stringLiteral [文字列型（const char*)]
+   */
   template <typename T,
             typename std::enable_if<std::is_same<T, const char*>::value>::type* = nullptr>
   Logger& operator<<(T stringLiteral)
