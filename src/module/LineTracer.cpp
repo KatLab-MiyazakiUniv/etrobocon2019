25a5e112922a6c66d55c652032f062fb08041308
Futa Hirakoba <korosuke613613@gmail.com>
2019-06-24 11:18:48 +0000

    turnValueがspeedValueを超えないようにした && LRコース27秒で完走する設定

--- src/module/LineTracer.cpp
+++ src/module/LineTracer.cpp
@@ -5,6 +5,7 @@
  */
 
 #include "LineTracer.h"
+#include "Logger.h"
 
 LineTracer::LineTracer(Controller& controller_, int targetBrightness_, bool isLeftCourse_)
   : controller(controller_),
@@ -14,6 +15,12 @@ LineTracer::LineTracer(Controller& controller_, int targetBrightness_, bool isLe
     speedControl(controller, 0.0, 0.0, 0.0, 0.0),
     turnControl(targetBrightness_, 0.0, 0.0, 0.0)
 {
+  Logger logger{ "w" };
+  logger << "brightness"
+         << "speed"
+         << "turn"
+         << "LEFT"
+         << "RIGHT";
 }
 
 void LineTracer::run(NormalCourseProperty& settings)
@@ -27,6 +34,7 @@ void LineTracer::run(NormalCourseProperty& settings)
   int speedValue = 0;                     // 直進値
   int leftPWM = 0;                        // 左モータの出力
   int rightPWM = 0;                       // 右モータの出力
+  Logger logger{ "a" };
 
   // 目標距離を走り終えるまでループ
   while(currentDistance - initialDistance < settings.targetDistance) {
@@ -36,7 +44,7 @@ void LineTracer::run(NormalCourseProperty& settings)
 
     // 旋回値の計算
     turnValue
-        = turnControl.calculateTurn(controller.getBrightness(), targetBrightness,
+        = turnControl.calculateTurn(speedValue, controller.getBrightness(), targetBrightness,
                                     settings.turnPid.Kp, settings.turnPid.Ki, settings.turnPid.Kd);
 
     // モータ出力の計算
@@ -49,7 +57,7 @@ void LineTracer::run(NormalCourseProperty& settings)
       leftPWM = speedValue + turnValue;
       rightPWM = speedValue - turnValue;
     }
-
+    logger << controller.getBrightness() << speedValue << turnValue << leftPWM << rightPWM;
     // PWM値の設定
     controller.setLeftMotorPwm(leftPWM);
     controller.setRightMotorPwm(rightPWM);
