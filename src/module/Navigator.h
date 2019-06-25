249f6f1fe252d8fa8ccdb3ddf18d234d0dcbe502
Oiwane Yuki <drhv8272@gmail.com>
2019-06-25 12:16:16 +0900

    Navigator.hのコメントを削除

--- src/module/Navigator.h
+++ src/module/Navigator.h
@@ -18,14 +18,14 @@ class Navigator {
  public:
   /**
    * コンストラクタ
-   * 
+   *
    * @brief Navigatorクラスのコンストラクタ
    * @param &controller_ [Controllerインスタンスの参照]
    */
   explicit Navigator(Controller& controller_, SpeedControl& speedControl_);
   /**
    * 前進と後進をする
-   * 
+   *
    * @brief specifiedDistanceの値でbackwardかforwardを呼び出す。
    * @param specifiedDistance [移動したい距離(mm)。正なら前進、負なら後進。]
    * @param pwm [モータの強さ]
@@ -34,7 +34,7 @@ class Navigator {
   void move(double specifiedDistance, int pwm = 30);
   /**
    * 指定した速度で前進と後進をする
-   * 
+   *
    * @brief specifiedDistanceとSpeedControl.calculateSpeedから戻ってきたPWMをmoveに渡す
    * @param specifiedDistance [移動したい距離(mm)]
    * @param specifiedSpeed [移動したい速度]
@@ -63,34 +63,12 @@ class Navigator {
    * @param dGain [PID制御のDゲイン]
    */
   void moveByPidAndSpeed(double specifiedDistance, int specifiedSpeed, const double pGain = 0.6,
-                 const double iGain = 0.0, const double dGain = 0.0);
+                         const double iGain = 0.0, const double dGain = 0.0);
 
  private:
   Distance distance;
   Controller& controller;
   SpeedControl& speedControl;
-  /**
-   * 前進する
-   * @brief モータを動かし、hasArrivedメソッドの戻り値がtrueの間前進する
-   * @note デフォルトでは両輪のPWM値に差はない。左右車輪で異なるPWM値をかけるときはalphaを指定する
-   * @param specifiedDistance [移動したい距離(mm)]
-   * @param goalDistance[現在地から移動したい距離動いた後の値(mm)]
-   * @param pwm[モータの強さ]
-   * @param alpha [左右のPWM値の差]
-   * @return なし
-   */
-  // void forward(double specifiedDistance, double goalDistance, int pwm, double alpha = 0.0);
-  /**
-   * 後進する
-   * @brief モータを動かし、hasArrivedメソッドの戻り値がtrueの間後進する
-   * @note デフォルトでは両輪のPWM値に差はない。左右車輪で異なるPWM値をかけるときはalphaを指定する
-   * @param specifiedDistance [移動したい距離(mm)]
-   * @param goalDistance[現在地から移動したい距離動いた後の値(mm)]
-   * @param pwm[モータの強さ]
-   * @param alpha [左右のPWM値の差]
-   * @return なし
-   */
-  // void backward(double specifiedDistance, double goalDistance, int pwm, double alpha = 0.0);
   /**
    * 指定した距離動いたか判定する
    * @brief 現在値とgoalDistanceを比較し、resultに"true"か"false"を格納する
