9169e22098535ca6034b1e5edd62b2bd7e3a1ed1
T.Miyaji <thomath0901@gmail.com>
2019-06-27 10:53:41 +0000

    Rotationクラスを作成した

---
+++ src/module/Rotation.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file   Rotation.cpp
+ * @brief  自転（走行体が両輪の中間を軸として回転）したときの角度を求めるクラス
+ * @author T.Miyaji
+ */
+#include "Rotation.h"
+
+Rotation::Rotation() : Radius(50), Tread(148) {}
+
+double Rotation::calculate(int leftAngle, int rightAngle)
+{
+  // 車輪の回転角度を自転角度に変換する係数
+  //! @see docs/odometry/odometry.pdf
+  const double transform = 2.0 * Radius / Tread;
+  double arc = distance.getDistance(std::abs(leftAngle), std::abs(rightAngle));
+
+  return transform * arc;
+}
