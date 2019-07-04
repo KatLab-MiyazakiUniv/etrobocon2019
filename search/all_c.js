b7628855cc45bed807fd005983c6be089a721e1d
katlabtab <katlabtab@gmail.com>
2019-07-04 04:02:54 +0000

    Update [ci skip]

--- search/all_c.js
+++ search/all_c.js
@@ -5,8 +5,6 @@ var searchData=
   ['makefile_2einc',['Makefile.inc',['../_makefile_8inc.html',1,'']]],
   ['motor_5fpwm_5fmax',['MOTOR_PWM_MAX',['../class_controller.html#a41bfe4231d282b5e15516d35ad327150',1,'Controller']]],
   ['motor_5fpwm_5fmin',['MOTOR_PWM_MIN',['../class_controller.html#a1d287382c61600efd21be5be50d9aaba',1,'Controller']]],
-  ['move',['move',['../class_navigator.html#ad3e9b0d9bb016b6d7ca0d7cd9f98bbcb',1,'Navigator']]],
-  ['moveatspecifiedspeed',['moveAtSpecifiedSpeed',['../class_navigator.html#a34f4d70ff245db32fee78e14c1212fd8',1,'Navigator']]],
-  ['moveatspecifiedspeedbypid',['moveAtSpecifiedSpeedByPid',['../class_navigator.html#ad6ded27d59d752cd9c51fe5dd525a9e3',1,'Navigator']]],
-  ['movebypid',['moveByPid',['../class_navigator.html#a093d0d47cdd93208f27301a02638b35e',1,'Navigator']]]
+  ['move',['move',['../class_navigator.html#ac6c06ea7120c771a786a2eaaedeadfd7',1,'Navigator']]],
+  ['moveatspecifiedspeed',['moveAtSpecifiedSpeed',['../class_navigator.html#a34f4d70ff245db32fee78e14c1212fd8',1,'Navigator']]]
 ];
