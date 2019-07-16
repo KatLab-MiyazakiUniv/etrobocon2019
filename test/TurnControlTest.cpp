2201d513c74cd30d5e044d23fe81b92d0a6172ec
Arima Kaoru <46063788+arimakaoru@users.noreply.github.com>
2019-07-16 11:37:02 +0900

    Update: 旋回値制御でローパスフィルターを使用するように変更

--- test/TurnControlTest.cpp
+++ test/TurnControlTest.cpp
@@ -7,29 +7,56 @@
 #include <gtest/gtest.h>
 
 namespace etrobocon2019_test {
-  TEST(TurnControl, TurnControl_init) { TurnControl turnCtrl(50, 0.6, 0.05, 0.04); }
+  TEST(TurnControl, TurnControl_init)
+  {
+    int targetBrightness = 50;
+    double Kp = 0.6;
+    double Ki = 0.05;
+    double Kd = 0.04;
+    TurnControl turnCtrl(targetBrightness, Kp, Ki, Kd);
+  }
 
   TEST(TurnControl, calculateTurn)
   {
-    TurnControl turnCtrl(50, 0.6, 0.05, 0.04);
+    int foward = 100;
+    int currentBrightness = 40;
+    int targetBrightness = 50;
+    double Kp = 0.6;
+    double Ki = 0.05;
+    double Kd = 0.04;
 
-    Pid pid(50.0, 0.6, 0.05, 0.04);
+    // 期待値を計算
+    Pid pid(static_cast<double>(targetBrightness), Kp, Ki, Kd);
+    Filter<> filter;
+    double filteredBrightness = filter.lowPassFilter(currentBrightness);
+    double expectedTurnValue = pid.control(filteredBrightness);
 
-    double expectedTurnValue = pid.control(70.0);
+    TurnControl turnCtrl(targetBrightness, Kp, Ki, Kd);
 
-    ASSERT_DOUBLE_EQ(expectedTurnValue, turnCtrl.calculateTurn(100, 70, 50, 0.6, 0.05, 0.04));
+    ASSERT_DOUBLE_EQ(expectedTurnValue, turnCtrl.calculateTurn(foward, currentBrightness,
+                                                               targetBrightness, Kp, Ki, Kd));
   }
 
-    TEST(TurnControl, limitCalculateTurn)
+  TEST(TurnControl, limitCalculateTurn)
   {
-    TurnControl turnCtrl(50, 0.6, 0.05, 0.04);
+    int foward = 50;
+    int currentBrightness = 40;
+    int targetBrightness = 50;
+    double Kp = 0.6;
+    double Ki = 0.05;
+    double Kd = 0.04;
 
-    Pid pid(50.0, 0.6, 0.05, 0.04);
+    // 期待値の計算
+    Pid pid(static_cast<double>(targetBrightness), Kp, Ki, Kd);
+    Filter<> filter;
+    double filteredBrightness = filter.lowPassFilter(currentBrightness);
+    double expectedTurnValue = pid.control(filteredBrightness) * foward / 100;
 
-    double expectedTurnValue = pid.control(70.0) * 50 / 100;
+    TurnControl turnCtrl(targetBrightness, Kp, Ki, Kd);
 
-    ASSERT_DOUBLE_EQ(expectedTurnValue, turnCtrl.calculateTurn(50, 70, 50, 0.6, 0.05, 0.04));
+    ASSERT_DOUBLE_EQ(expectedTurnValue, turnCtrl.calculateTurn(foward, currentBrightness,
+                                                               targetBrightness, Kp, Ki, Kd));
   }
-}  
+}  // namespace etrobocon2019_test
 
 // namespace etrobocon2019_test
