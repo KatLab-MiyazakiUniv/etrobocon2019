2d5641000c39dcd67e411766e267a52f1e72ceb0
katlabtab <katlabtab@gmail.com>
2019-07-29 02:24:11 +0000

    Update [ci skip]

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['hsvstatus',['HsvStatus',['../struct_hsv_status.html',1,'']]],
-  ['hsvtocolor',['hsvToColor',['../class_controller.html#ac9babd9f17deef86759153fc1f1ee511',1,'Controller']]],
+  ['hsvtocolor',['hsvToColor',['../class_controller.html#add5d70e5a1780660ea7d8766391bb41d',1,'Controller']]],
   ['hue',['hue',['../struct_hsv_status.html#add1324a6ecb8cf292dcfdb2e9f2579cb',1,'HsvStatus']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hsvtocolor',['hsvToColor',['../class_controller.html#ac9babd9f17deef86759153fc1f1ee511',1,'Controller']]]
+  ['hsvtocolor',['hsvToColor',['../class_controller.html#add5d70e5a1780660ea7d8766391bb41d',1,'Controller']]]
 ];
