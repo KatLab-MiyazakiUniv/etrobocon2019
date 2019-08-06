a5201881ab9d3d53fe907eeb4b8a4097980bc491
Oiwane Yuki <drhv8272@gmail.com>
2019-08-06 14:48:08 +0900

    BlockBingoTestの修正

--- test/BlockBingoTest.cpp
+++ test/BlockBingoTest.cpp
@@ -34,27 +34,10 @@ namespace etrobocon2019_test {
     int leftAngle = controller.getLeftMotorCount();
     int rightAngle = controller.getRightMotorCount();
 
-    double actualDistance = distance.getDistance(leftAngle, rightAngle);
-    double expectedDistance = 350.0;
+    double actual = distance.getDistance(leftAngle, rightAngle);
+    double expected = 350.0;
 
-    ASSERT_LE(expectedDistance, actualDistance);
-    ASSERT_LE(actualDistance, expectedDistance + 5.0);
-
-    propertys = { { Order::SPIN, 45.0, Color::black } };
-    blockbingo.execOrder<1>(propertys);
-    // 特にテストなし
-
-    Color expectedColor = Color::yellow;
-    propertys = { { Order::COLOR, 0.0, expectedColor } };
-    blockbingo.execOrder<1>(propertys);
-
-    int r = 0;
-    int g = 0;
-    int b = 0;
-    controller.getRawColor(r, g, b);
-    controller.convertHsv(r, g, b);
-    Color actualColor = controller.hsvToColor(controller.getHsv());
-
-    ASSERT_EQ(expectedColor, actualColor);
+    ASSERT_LE(expected, actual);
+    ASSERT_LE(actual, expected + 5.0);
   }
 }  // namespace etrobocon2019_test
