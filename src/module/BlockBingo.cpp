a7baaac94f4e0e1d158e59e97194d8de165a8b22
Oiwane Yuki <drhv8272@gmail.com>
2019-09-23 16:42:41 +0900

    走行体動作定義に合わせて走行体が動くようにする関数を作成

--- src/module/BlockBingo.cpp
+++ src/module/BlockBingo.cpp
@@ -5,16 +5,193 @@
  */
 #include "BlockBingo.h"
 
-BlockBingo::BlockBingo(Controller& controller_) : controller(controller_) {}
+BlockBingo::BlockBingo(Controller& controller_)
+  : controller(controller_), length(175.0), isFirstProcess(true)
+{
+  this->initMap();
+}
 
 void BlockBingo::moveCircle4OfL()
 {
   Navigator navigator(controller);
 
-  // 色識別が上手くいったら下のコメントを処理に加える
-  // navigator.moveToSpecifiedColor(Color::yellow, 10);
-  navigator.move(-90.0, 10);
+  navigator.moveToSpecifiedColor(Color::yellow, 10);
+  navigator.move(-65.0, 10, 0.6);
+  navigator.spin(45.0, false, 10);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, true, 10);
+  navigator.move(length, 10, 0.6);
+}
+
+void BlockBingo::moveCircle6OfL()
+{
+  Navigator navigator(controller);
+
+  navigator.moveToSpecifiedColor(Color::yellow, 10);
+  navigator.move(-65.0, 10);
+  navigator.spin(45.0, true);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, false);
+  navigator.move(length, 10, 0.6);
+}
+
+void BlockBingo::execSpinRight90()
+{
+  Navigator navigator(controller);
+
+  navigator.spin(90.0, true);
+}
+
+void BlockBingo::execSpinLeft90()
+{
+  Navigator navigator(controller);
+
+  navigator.spin(90.0, false);
+}
+
+void BlockBingo::execSpin180()
+{
+  Navigator navigator(controller);
+
+  navigator.spin(180, true);
+}
+
+void BlockBingo::execStraight()
+{
+  Navigator navigator(controller);
+
+  navigator.move(length * 2, 10, 0.6);
+}
+
+void BlockBingo::execStraightDetourRight()
+{
+  Navigator navigator(controller);
+
+  navigator.spin(45.0, true);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(90.0, false);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, true);
+}
+
+void BlockBingo::execStraightDetourLeft()
+{
+  Navigator navigator(controller);
+
   navigator.spin(45.0, false);
   navigator.moveToSpecifiedColor(Color::black, 10);
-  navigator.spin(45.0);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(90.0, true);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, false);
+}
+
+void BlockBingo::execTurnRight90ExistBlock()
+{
+  Navigator navigator(controller);
+
+  navigator.spin(45.0, true);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, true);
+}
+
+void BlockBingo::execTurnRight90UnexistBlock()
+{
+  Navigator navigator(controller);
+
+  navigator.move(length, 10, 0.6);
+  this->execSpinRight90();
+  navigator.move(length, 10, 0.6);
+}
+
+void BlockBingo::execTurnLeft90ExistBlock()
+{
+  Navigator navigator(controller);
+
+  navigator.spin(45.0, false);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, false);
+}
+
+void BlockBingo::execTurnLeft90UnexistBlock()
+{
+  Navigator navigator(controller);
+
+  navigator.move(length, 10, 0.6);
+  this->execSpinLeft90();
+  navigator.move(length, 10, 0.6);
+}
+
+void BlockBingo::execTurn180()
+{
+  this->execSpin180();
+  this->execStraight();
+}
+
+void BlockBingo::execTurnRight180()
+{
+  Navigator navigator(controller);
+
+  this->execSpin180();
+  navigator.spin(45.0, true);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  this->execSpinLeft90();
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, true);
+}
+
+void BlockBingo::execTurnLeft180()
+{
+  Navigator navigator(controller);
+
+  this->execSpin180();
+  navigator.spin(45.0, false);
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  this->execSpinRight90();
+  navigator.moveToSpecifiedColor(Color::black, 10);
+  navigator.move(50.0, 10, 0.6);
+  navigator.spin(45.0, false);
+}
+
+void BlockBingo::execPut()
+{
+  Navigator navigator(controller);
+
+  if(isFirstProcess) {
+    navigator.move(length, 10, 0.6);
+    isFirstProcess = false;
+  }
+  navigator.move(100.0, 10, 0.6);
+  navigator.move(-100.0, 10, 0.6);
+}
+
+void BlockBingo::initMap()
+{
+  mp["a"] = Order::ENTER_BINGO_AREA_L4;
+  mp["b"] = Order::ENTER_BINGO_AREA_L6;
+  mp["c"] = Order::STRAIGHT;
+  mp["d"] = Order::SPIN_RIGHT90;
+  mp["e"] = Order::SPIN_LEFT90;
+  mp["f"] = Order::SPIN180;
+  mp["g"] = Order::PUT;
+
+  mp["h"] = Order::STRAIGHT_DETOUR_RIGHT;
+  mp["i"] = Order::STRAIGHT_DETOUR_LEFT;
+  mp["j"] = Order::TURN_RIGHT90_EXIST_BLOCK;
+  mp["k"] = Order::TURN_RIGHT90_UNEXIST_BLOCK;
+  mp["l"] = Order::TURN_LEFT90_EXIST_BLOCK;
+  mp["m"] = Order::TURN_LEFT90_UNEXIST_BLOCK;
+  mp["n"] = Order::TURN180;
+  mp["o"] = Order::TURN_RIGHT180;
+  mp["p"] = Order::TURN_LEFT180;
 }
--- src/module/BlockBingo.h
+++ src/module/BlockBingo.h
@@ -7,25 +7,110 @@
 #define BLOCKBINGO_H
 
 #include <array>
+#include <map>
 #include "Controller.h"
 #include "Navigator.h"
 
-enum class Order { MOVE, SPIN, COLOR };
-
-/**
- * execOrderで使用する構造体
- *
- * paramValueまたはparamColorを使用しないときは何か値を入れておく
- */
-struct OrderProperty {
-  Order orderName;    //命令の内容
-  double paramValue;  // Navigator.spinの引数で使用する
-  Color paramColor;   // Navigator.moveToSpecifiedColorの引数で使用する
+enum class Order {
+  ENTER_BINGO_AREA_L4,
+  ENTER_BINGO_AREA_L6,
+  SPIN_RIGHT90,
+  SPIN_LEFT90,
+  SPIN180,
+  STRAIGHT,
+  STRAIGHT_DETOUR_RIGHT,
+  STRAIGHT_DETOUR_LEFT,
+  TURN_RIGHT90_EXIST_BLOCK,
+  TURN_RIGHT90_UNEXIST_BLOCK,
+  TURN_LEFT90_EXIST_BLOCK,
+  TURN_LEFT90_UNEXIST_BLOCK,
+  TURN180,
+  TURN_RIGHT180,
+  TURN_LEFT180,
+  PUT,
 };
 
 class BlockBingo {
  private:
   Controller& controller;
+  //交点サークルから中点までの距離
+  const double length;
+  // FirstProcess = ブロックサークル内の黒ブロックをボーナスサークル内に設置する
+  bool isFirstProcess;
+  std::map<std::string, Order> mp;
+
+  //ここからのprivate関数の詳細はモデルの2.2を参照
+  /**
+   * SPIN_RIGHT90の命令を実行する
+   * @brief 90度右に回頭する
+   */
+  void execSpinRight90();
+  /**
+   * SPIN_LEFT90の命令を実行する
+   * @brief 90度左に回頭する
+   */
+  void execSpinLeft90();
+  /**
+   * SPIN180の命令を実行する
+   * @brief 180度回頭する
+   */
+  void execSpin180();
+  /**
+   * STRAIGHTの命令を実行する
+   * @brief
+   * 進行方向にブロックがない場合に使用する。ある交点サークルから隣接する交点サークルの距離の分だけ移動する
+   */
+  void execStraight();
+  /**
+   * STRAIGHT_DETOUR_RIGHTの命令を実行する
+   * @brief 進行方向にブロックがある場合に使用する。右に迂回するように直進する
+   */
+  void execStraightDetourRight();
+  /**
+   * STRAIGHT_DETOUR_RIGHTの命令を実行する
+   * @brief 進行方向にブロックがある場合に使用する。左に迂回するように直進する
+   */
+  void execStraightDetourLeft();
+  /**
+   * TURN_RIGHT90_EXIST_BLOCKの命令を実行する
+   * @brief ブロックのある交点サークルで右に90度曲がる。
+   */
+  void execTurnRight90ExistBlock();
+  /**
+   * TURN_RIGHT90_UNEXIST_BLOCKの命令を実行する
+   * @brief ブロックのない交点サークルで右に90度曲がる。
+   */
+  void execTurnRight90UnexistBlock();
+  /**
+   * TURN_LEFT90_EXIST_BLOCKの命令を実行する
+   * @brief ブロックのある交点サークルで左に90度曲がる。
+   */
+  void execTurnLeft90ExistBlock();
+  /**
+   * TURN_LEFT90_UNEXIST_BLOCKの命令を実行する
+   * @brief ブロックのない交点サークルで左に90度曲がる。
+   */
+  void execTurnLeft90UnexistBlock();
+  /**
+   * TURN180の命令を実行する
+   * @brief ブロックがない場合に使用される。中点で180度旋回してから交点サークルまで直進する
+   */
+  void execTurn180();
+  /**
+   * TURN_RIGHT180の命令を実行する
+   * @brief ブロックがある場合に使用される。中点で180度旋回してから右に迂回して直進する
+   */
+  void execTurnRight180();
+  /**
+   * TURN_RIGHT180の命令を実行する
+   * @brief ブロックがある場合に使用される。中点で180度旋回してから左に迂回して直進する
+   */
+  void execTurnLeft180();
+  /**
+   * PUTの命令を実行する
+   * @brief アームを上げ下げしてブロックをブロックサークルに設置する
+   */
+  void execPut();
 
  public:
   /**
@@ -40,26 +125,57 @@ class BlockBingo {
    * 黒線の中点まで斜めに移動し、ブロックサークル4の中央まで移動する。
    */
   void moveCircle4OfL();
-
+  /**
+   * Lコースのブロックビンゴのブロックサークル6に移動する
+   * @brief Lコースのブロックビンゴ開始点からブロックサークル6の手前の
+   * 黒線の中点まで斜めに移動し、ブロックサークル6の中央まで移動する。
+   */
+  void moveCircle6OfL();
+  /**
+   * 命令に使用するマップを初期化する
+   */
+  void initMap();
   /**
    * パソコンから受け取ったリストの通りに処理を実行する
-   * @param propertys [命令の情報のリスト]
+   * @param orders [命令の情報のリスト]
    */
   template <int N>
-  void execOrder(std::array<OrderProperty, N>& propertys)
+  void execOrder(std::array<std::string, N>& orders)
   {
-    Navigator navigator(controller);
-
-    for(const auto& property : propertys) {
-      if(property.orderName == Order::MOVE) {
-        navigator.move(350.0, 10, 0.6);
-      } else if(property.orderName == Order::SPIN) {
-        navigator.spin(property.paramValue, (property.paramValue > 0));
-      } else if(property.orderName == Order::COLOR) {
-        navigator.moveToSpecifiedColor(property.paramColor, 10);
-      } else {
-        controller.stopMotor();
-        controller.speakerPlayToneFS6(100);
+    for(const auto& orderKey : orders) {
+      decltype(mp)::iterator it = mp.find(orderKey);
+      if(it->second == Order::ENTER_BINGO_AREA_L4) {
+        this->moveCircle4OfL();
+      } else if(it->second == Order::ENTER_BINGO_AREA_L6) {
+        this->moveCircle6OfL();
+      } else if(it->second == Order::SPIN_RIGHT90) {
+        this->execSpinRight90();
+      } else if(it->second == Order::SPIN_LEFT90) {
+        this->execSpinLeft90();
+      } else if(it->second == Order::SPIN180) {
+        this->execSpin180();
+      } else if(it->second == Order::STRAIGHT) {
+        this->execStraight();
+      } else if(it->second == Order::STRAIGHT_DETOUR_RIGHT) {
+        this->execStraightDetourRight();
+      } else if(it->second == Order::STRAIGHT_DETOUR_LEFT) {
+        this->execStraightDetourLeft();
+      } else if(it->second == Order::TURN_RIGHT90_EXIST_BLOCK) {
+        this->execTurnRight90ExistBlock();
+      } else if(it->second == Order::TURN_RIGHT90_UNEXIST_BLOCK) {
+        this->execTurnRight90UnexistBlock();
+      } else if(it->second == Order::TURN_LEFT90_EXIST_BLOCK) {
+        this->execTurnLeft90ExistBlock();
+      } else if(it->second == Order::TURN_LEFT90_UNEXIST_BLOCK) {
+        this->execTurnLeft90UnexistBlock();
+      } else if(it->second == Order::TURN180) {
+        this->execTurn180();
+      } else if(it->second == Order::TURN_RIGHT180) {
+        this->execTurnRight180();
+      } else if(it->second == Order::TURN_LEFT180) {
+        this->execTurnLeft180();
+      } else if(it->second == Order::PUT) {
+        this->execPut();
       }
     }
   }
