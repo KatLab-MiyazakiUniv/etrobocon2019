7aab356924eb21537ed29676a523553303a1a648
M.Matsuura <hm16040@student.miyazaki-u.ac.jp>
2019-10-28 14:01:13 +0900

    表示するコメントの変更

--- src/module/Calibrator.cpp
+++ src/module/Calibrator.cpp
@@ -16,7 +16,7 @@ Calibrator::Calibrator(Controller& controller_)
 
 bool Calibrator::calibration()
 {
-  Display::print(2, "Please push left button(^-^)");
+  Display::print(2, "Please push left touch sensor...");
   this->setArm();
   Display::print(2, "now calibration...");
 
