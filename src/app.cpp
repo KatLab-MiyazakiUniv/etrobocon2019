9471b59a9c59928c5a9eff06c44cc07cfa4e4497
T.Miyaji <thomath0901@gmail.com>
2019-11-06 17:53:51 +0900

    app.cfg類に自己位置の情報をcsvに出力する機能を追加した

--- src/app.cpp
+++ src/app.cpp
@@ -2,7 +2,10 @@
 #include "EtRobocon2019.h"
 #include "Bluetooth.h"
 #include "Controller.h"
+#include "Logger.h"
+#include "Distance.h"
 #include <array>
+#include <cmath>
 
 // 演習用のユーティリティ
 std::array<char, 256> Bluetooth::commands;
@@ -26,6 +29,44 @@ void main_task(intptr_t unused)
   ext_tsk();
 }
 // end::main_task_2[]
+
+void localization_task(intptr_t unused)
+{
+  Controller controller;
+  Logger log("local.csv");
+  Distance distance;
+
+  int left_old = 0;
+  int right_old = 0;
+  double x = 0.0;
+  double y = 0.0;
+  double theta = 0.0;
+  constexpr double Tread = 130.0;  // トレッド幅
+
+  while(true) {
+    int left = controller.getLeftMotorCount();
+    int right = controller.getRightMotorCount();
+    // 走行体の速度を算出
+    double velocity = distance.getDistance(left - left_old, right - right_old);
+    // 走行体の旋回角度を算出
+    double omega = ((right - right_old) - (left - left_old)) / Tread;
+
+    x += velocity * std::cos(theta + (omega / 2.0));  // x座標
+    y += velocity * std::sin(theta + (omega / 2.0));  // y座標
+    theta += omega;
+
+    log << left    // 左モーターの回転量
+        << right   // 右モーターの回転量
+        << x       // x座標
+        << y       // y座標
+        << theta;  // 姿勢（角度）
+
+    left_old = left;
+    right_old = right;
+    controller.tslpTsk(4);
+  }
+  ext_tsk();
+}
 
 void color_task(intptr_t unused)
 {
