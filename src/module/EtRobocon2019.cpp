ff91c54c329a003ac64040e0793fcd1bdf748e06
T.Miyaji <thomath0901@gmail.com>
2019-11-10 20:47:25 +0900

    CurvatureのPID値を調整中

--- src/module/EtRobocon2019.cpp
+++ src/module/EtRobocon2019.cpp
@@ -8,6 +8,9 @@
 #include "Navigator.h"
 #include "Parking.h"
 #include "MoveDirectGarage.h"
+#include "Distance.h"
+#include "Curvature.h"
+#include "Logger.h"
 
 void EtRobocon2019::start()
 {
@@ -26,28 +29,23 @@ void EtRobocon2019::start()
   }
   Bluetooth::is_start = true;
 
-  NormalCourse normalCourse(controller, isLeftCourse, targetBrightness);
-  // NormalCourseを走り出す．
-  normalCourse.runNormalCourse();
-
-  // ブロックビンゴ
-  BlockBingo blockBingo(controller, targetBrightness);
-  // ここでビンゴを開始するblockBingoのメンバ関数を呼び出す
-  blockBingo.execOrder<256>(Bluetooth::commands);
-
-  //直接ガレージに移動する
-  MoveDirectGarage moveDirectGarage(controller ,targetBrightness);
-  if(isLeftCourse) {
-  //ブロックビンゴを実行する処理を記述
-  }else{
-    moveDirectGarage.moveDirectGarageR();//Rコースの場合はビンゴを行わずにガレージ駐車を行う
+  while(!controller.touchSensor.isPressed()) {
+    controller.tslpTsk(4);
   }
 
-  // ガレージ
-  Parking parking(controller, targetBrightness);
-  if(isLeftCourse) {
-    parking.parkAtAL();
-  } else {
-    parking.parkAtAR();
+  double distances[] = { 750, 640 };
+  double curvatures[] = { 0.0, 0.00255 };
+  for(int i = 0; i != 2; ++i) {
+    Curvature curvature(curvatures[i], 1.0, 1.8, 0.0);
+    Distance distance;
+    while(distance.getDistance(controller.getLeftMotorCount(), controller.getRightMotorCount())
+          < distances[i]) {
+      int turnValue
+          = curvature.control(controller.getLeftMotorCount(), controller.getRightMotorCount(), 640);
+      controller.setLeftMotorPwm(70 - turnValue);
+      controller.setRightMotorPwm(70 + turnValue);
+
+      controller.tslpTsk(4);
+    }
   }
 }
