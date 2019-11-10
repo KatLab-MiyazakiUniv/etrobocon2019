182a7da0d3d7ad399da4dde26cfe69c3c9f9d547
Oiwane <44086176+Oiwane@users.noreply.github.com>
2019-11-10 15:49:54 +0900

    テストコードのインデントが直ったはず

--- test/CurvatureTest.cpp
+++ test/CurvatureTest.cpp
@@ -8,42 +8,42 @@
 
 namespace etrobocon2019_test
 {
-	TEST(Curvature, Curvature_init)
-	{
-		double target = 0.010;
-		Curvature curvature(target, 1.0, 0.0, 0.0);
-	}
+  TEST(Curvature, Curvature_init)
+  {
+    double target = 0.010;
+    Curvature curvature(target, 1.0, 0.0, 0.0);
+  }
 
-	TEST(Curvature, controlTest)
-	{
-		Controller controller;
-		double target = 0.0;
-		Curvature curvature(target, 1.0, 0.0, 0.0);
+  TEST(Curvature, controlTest)
+  {
+    Controller controller;
+    double target = 0.0;
+    Curvature curvature(target, 1.0, 0.0, 0.0);
 
-		double pidValue
-			= curvature.control(controller.getLeftMotorCount(), controller.getRightMotorCount());
+    double pidValue
+      = curvature.control(controller.getLeftMotorCount(), controller.getRightMotorCount());
 
-		ASSERT_DOUBLE_EQ(0.0, pidValue);
-	}
+    ASSERT_DOUBLE_EQ(0.0, pidValue);
+  }
 
-	TEST(Curvature, controlTest2)
-	{
-		Controller controller;
-		double target = 0.0;
-		Curvature curvature(target, 1.0, 0.0, 0.0);
+  TEST(Curvature, controlTest2)
+  {
+    Controller controller;
+    double target = 0.0;
+    Curvature curvature(target, 1.0, 0.0, 0.0);
 
-		for (int i = 0; i < 10; i++)
-		{
-			int pwm = 30;
-			controller.setLeftMotorPwm(pwm);
-			controller.setRightMotorPwm(pwm - 1);
-			controller.tslpTsk(4);
-		}
+    for (int i = 0; i < 10; i++)
+    {
+      int pwm = 30;
+      controller.setLeftMotorPwm(pwm);
+      controller.setRightMotorPwm(pwm - 1);
+      controller.tslpTsk(4);
+    }
 
-		double pidValue
-			= curvature.control(controller.getLeftMotorCount(), controller.getRightMotorCount());
+    double pidValue
+      = curvature.control(controller.getLeftMotorCount(), controller.getRightMotorCount());
 
-		ASSERT_LE(0.0, pidValue);
-		ASSERT_LE(pidValue, 1.0);
-	}
+    ASSERT_LE(0.0, pidValue);
+    ASSERT_LE(pidValue, 1.0);
+  }
 }// namespace etrobocon2019_test
