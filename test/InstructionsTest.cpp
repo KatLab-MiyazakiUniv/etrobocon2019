51f268ee238c8143cc0e759c240fe10964a1d39f
Oiwane <hm16007@student.miyazaki-u.ac.jp>
2019-11-18 08:01:46 +0900

    テストコードを追加した

--- test/InstructionsTest.cpp
+++ test/InstructionsTest.cpp
@@ -16,8 +16,6 @@ namespace etrobocon2019_test {
 
     ASSERT_EQ(Order::ENTER_BINGO_AREA_L4, instructions.translate('a'));
     ASSERT_EQ(Order::ENTER_BINGO_AREA_L6, instructions.translate('b'));
-    ASSERT_EQ(Order::ENTER_BINGO_AREA_R5, instructions.translate('w'));
-    ASSERT_EQ(Order::ENTER_BINGO_AREA_R8, instructions.translate('x'));
     ASSERT_EQ(Order::STRAIGHT, instructions.translate('c'));
     ASSERT_EQ(Order::SPIN_RIGHT, instructions.translate('d'));
     ASSERT_EQ(Order::SPIN_LEFT, instructions.translate('e'));
@@ -32,7 +30,12 @@ namespace etrobocon2019_test {
     ASSERT_EQ(Order::TURN180, instructions.translate('n'));
 
     ASSERT_EQ(Order::PREPARE_TO_PUT, instructions.translate('o'));
+    ASSERT_EQ(Order::STRAIGHT_STRAIGHT, instructions.translate('p'));
+    ASSERT_EQ(Order::MOVE_TO_MIDPOINT, instructions.translate('q'));
     ASSERT_EQ(Order::MOVE_NODE, instructions.translate('u'));
+
+    ASSERT_EQ(Order::ENTER_BINGO_AREA_R5, instructions.translate('w'));
+    ASSERT_EQ(Order::ENTER_BINGO_AREA_R8, instructions.translate('x'));
 
     ASSERT_EQ(Order::QUICK_PUT_R, instructions.translate('y'));
     ASSERT_EQ(Order::QUICK_PUT_L, instructions.translate('z'));
